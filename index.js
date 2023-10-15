796e702d7e181483955b94e1fa820a24543ff0f8
Bunkei-Engineer-Shun <147960506+Bunkei-Engineer-Shun@users.noreply.github.com>
2023-10-15 12:18:18 +0900

    編集済み

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ playBtn.addEventListener("click", playBgm);
 // BGM再生・停止関数
 function playBgm() {
   if (icon.textContent === "music_note") {
+    // BGMを再生する
     bgm.play();
     // BGMの設定(音量MAX、ループされる)
     bgm.volume = 1;
     bgm.loop = true;
+    // アイコンを変更する
     icon.textContent = "stop_circle";
+    // 再生中のスタイルを変更するためにクラス名を追加
+    playBtn.classList.add("playing");
   } else if (icon.textContent === "stop_circle") {
+    // BGMを停止する
     bgm.pause();
+    // アイコンを変更する
     icon.textContent = "music_note";
+    // クラス名を外す
+    playBtn.classList.remove("playing");
   }
 }
